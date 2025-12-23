34ac74b8d3aa95060103ec66830ea16ebafc48a3
utsav1103 <utsavu858@gmail.com>
2025-12-23 17:23:11 +0530

    assuring the tailwind is working

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,4 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,7 +8,12 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main>
-    
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Application & Resume Ratings</h1>
+        <h2>Review and check AI-Powered feedback.</h2>
+      </div>
+    </section>
   </main>
 }
