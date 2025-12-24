75fe517ed23070ab7a4d10a5fa54210681a1bd9e
utsav1103 <utsavu858@gmail.com>
2025-12-24 20:05:37 +0530

    fixing typo in navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,15 @@
+import { Link } from "react-router"
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+        <Link to="/">
+        <p className="text-2xl font-bold text-gradient">RESUMEISE</p>
+        </Link>
+        <Link to="/upload" className="primary-button w-fit">
+            Upload Resume
+        </Link>
+    </nav>
   )
 }
 
