30bba407654ba049f38240981f6bb8f9b476b9b3
utsav1103 <utsavu858@gmail.com>
2025-12-25 20:07:46 +0530

    auth login and logout

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { usePuterStore } from "~/lib/puter"
 
-const auth = () => {
-  return (
-    <div>auth</div>
-  )
+export const meta = () => ([
+    { title: 'Resumeise | Auth' },
+    { name: 'description', content: "Log into your account" },
+])
+
+const Auth = () => {
+    const { isLoading, auth } = usePuterStore();
+    return (
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+
+            <div className="gradient-border shadow-lg">
+                <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                    <div className="flex flex-col intems-center gap-2 text-center">
+                        <h1>Welcome</h1>
+                        <h2>Log In to Continue Your Job Journey</h2>
+                    </div>
+                    <div>
+                        {isLoading ? (
+                            <button className="auth-button animate-pulse">
+                                <p>Signing you in...</p>
+                            </button>
+                        ) : (<>
+                            {auth.isAuthenticated ? (
+                                <button className="auth-button" onClick={auth.signOut}>
+                                    <p>Log Out</p>
+                                </button>
+                            ) : (
+                                <button className="auth-button" onClick={auth.signIn}>
+                                    <p>Login</p>
+                                </button>
+                            )}
+                        </>
+                        )}
+                    </div>
+                </section>
+            </div>
+
+        </main>
+    )
 }
 
-export default auth
+export default Auth
