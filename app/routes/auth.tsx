a70b938f451de291640d2fff65a7a976014edf34
utsav1103 <utsavu858@gmail.com>
2025-12-26 11:15:53 +0530

    isloading and next navigation

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
 import { usePuterStore } from "~/lib/puter"
 
 export const meta = () => ([
@@ -7,6 +9,14 @@ export const meta = () => ([
 
 const Auth = () => {
     const { isLoading, auth } = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (auth.isAuthenticated) navigate(next);
+    }, [auth.isAuthenticated, next])
+
     return (
         <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
 
