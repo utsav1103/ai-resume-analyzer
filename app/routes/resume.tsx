ee830ebff81f0ad1619ccf9545d3b625023d8edd
utsav1103 <utsavu858@gmail.com>
2025-12-29 22:22:30 +0530

    fetching the resume for puter

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,12 +1,47 @@
-import { Link, useParams } from "react-router"
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router"
+import { usePuterStore } from "~/lib/puter";
 
 export const meta = () => ([
     { title: 'Resumeise | Review' },
     { name: 'description', content: "Deatiled overview" },
 ])
 const Resume = () => {
+    const {auth, isLoading , fs, kv} = usePuterStore();
     //destructuring the id 
     const { id } = useParams();
+    const [imageURl , setImageUrl] = useState('');
+    const [ resumeUrl, setResumeUrl] = useState('');
+    const [ feedback, setFeedback] = useState('');
+    const navigate = useNavigate();
+
+    useEffect (()=>{
+        const loadResume = async ()=> {
+            const resume = await kv.get(`resume:${id}`);
+
+            if(!resume) return;
+
+            const data = JSON.parse(resume);
+
+            const resumeBlob = await fs.read(data.resumePath);
+            if(!resumeBlob) return;
+
+            const pdfBlob = new Blob([resumeBlob],{type:'application/pdf'});
+            const resumeUrl = URL.createObjectURL(pdfBlob);
+            setResumeUrl(resumeUrl);
+
+            const imageBlob = await fs.read(data.imagePath);
+            if(!imageBlob) return;
+            const imageUrl = URL.createObjectURL(imageBlob);
+            setImageUrl(imageUrl);
+
+            setFeedback(data.feedback);
+            console.log({resumeUrl,imageURl,feedback:data.feedback});
+
+        }
+        loadResume();
+    },[id]
+);
     return (
         <main className="!pt-0">
             <nav className="resume-nav">
@@ -15,6 +50,19 @@ const Resume = () => {
                     <span className=" text-gray-800 text-sm font-semibol">Back to Homepage</span>
                 </Link>
             </nav>
+            <div className="flex flex-row w-full max-lg:flex-col-reverse">
+                <section className="feedback-section bg-[url('/images/bg-small.svg')] bg-cover h-[100vh] sticky top-0 items-center justify-center">
+                {imageURl && resumeUrl && (
+                    <div className="animate-in fade-in duration-1000 gradient-boarder max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
+                        <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
+                            <img 
+                            src={imageURl}
+                            className="w-full h-full object-contain rounded-2xl" title="resume"/>
+                        </a>
+                    </div>
+                )}
+                </section>
+            </div>
 
         </main>
     )
