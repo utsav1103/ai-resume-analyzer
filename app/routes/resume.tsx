e358806ed8439576f08e151c06979690c683db0e
utsav1103 <utsavu858@gmail.com>
2025-12-29 21:53:16 +0530

    adding icon and back to home page

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Link, useParams } from "react-router"
 
+export const meta = () => ([
+    { title: 'Resumeise | Review' },
+    { name: 'description', content: "Deatiled overview" },
+])
 const Resume = () => {
-  return (
-    <div>Resume</div>
-  )
+    //destructuring the id 
+    const { id } = useParams();
+    return (
+        <main className="!pt-0">
+            <nav className="resume-nav">
+                <Link to="/" className='back-button'>
+                    <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5"/>
+                    <span className=" text-gray-800 text-sm font-semibol">Back to Homepage</span>
+                </Link>
+            </nav>
+
+        </main>
+    )
 }
 
 export default Resume
