319b273f9275d1989959dbca957c364c853b0131
utsav1103 <utsavu858@gmail.com>
2026-01-02 19:45:34 +0530

    finishing the score summary

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ScoreGauge from './ScoreGauge'
+import ScoreBadge from '~/components/ScoreBadge';
 
 const Category = ({ title, score }: { title: string, score: number }) => {
   const textColor = score > 70 ? 'text-green-600' :
@@ -10,6 +11,7 @@ const Category = ({ title, score }: { title: string, score: number }) => {
       <div className='category'>
         <div className='flex flex-row gap-2 items-center justify-center'>
           <p className='text-2xl'>{title}</p>
+          <ScoreBadge score={score}/>
         </div>
         <p className='text-2xl'>
           <span className={textColor}>{score}</span>/100
