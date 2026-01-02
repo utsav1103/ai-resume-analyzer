fe6f867e8e73b5e73ee259669da261f68bb2a54a
utsav1103 <utsavu858@gmail.com>
2026-01-02 19:19:01 +0530

    importing scoreGauge in summary

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import ScoreGauge from './ScoreGauge'
 
-const Summary = () => {
+const Summary = ({feedback}:{feedback:Feedback}) => {
   return (
-    <div>Summary</div>
+    <div className='bg-white rounded-2xl shadow-md w-full'>
+        <div className='flex flex-row items-center p-4 gap-8'>
+            <ScoreGauge score={feedback.overallScore}/>
+        </div>
+    </div>
   )
 }
 
